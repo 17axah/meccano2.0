58619848cdf740e13bf14b39ec06434519ff716c
17axaH <17axah@mail.ru>
2024-08-20 01:01:02 +0600

    fix images task

--- services/tasks/images.js
+++ services/tasks/images.js
@@ -3,7 +3,7 @@ import config from '../config.js'
 import newer from 'gulp-newer'
 
 export const images = () => {
-  return src(config.images.src)
+  return src(config.images.src, { encoding: false })
     .pipe(newer(config.images.dest))
     .pipe(dest(config.images.dest))
 }
