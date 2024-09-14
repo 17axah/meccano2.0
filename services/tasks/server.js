aa17f198149a9ab2522553fec3c82ba11bc22d23
17axaH <17axah@mail.ru>
2024-09-14 17:38:19 +0600

    fix server task

--- services/tasks/server.js
+++ services/tasks/server.js
@@ -4,14 +4,11 @@ import chalk from 'chalk';
 import qrcode from 'qrcode-terminal';
 import os from 'os';
 
-function getNetworkIp() {
-  const interfaces = os.networkInterfaces().Ethernet
-
-  return interfaces.find((item) => item.family === 'IPv4')?.address
-}
+const interfaces = Object.values(os.networkInterfaces() || {}).flat()
+const networkIp = interfaces.find((item) => item.address.includes('192.168.0'))?.address
 
 const LOCAL_URL = `http://127.0.0.1:${config.port}`
-const NETWORK_URL = `http://${getNetworkIp()}:${config.port}`
+const NETWORK_URL = networkIp ? `http://${networkIp}:${config.port}` : 'unknown'
 
 export const server = (done) => {
   liveServer.start({
@@ -24,9 +21,11 @@ export const server = (done) => {
 
   setTimeout(() => {
     console.log(chalk.blue('Local: ') + chalk.blue.underline(LOCAL_URL));
-    console.log(chalk.gray('Network: ') + chalk.gray.underline(NETWORK_URL))
 
-    qrcode.generate(NETWORK_URL, { small: true });
+    if (networkIp) {
+      console.log(chalk.gray('Network: ') + chalk.gray.underline(NETWORK_URL))
+      qrcode.generate(NETWORK_URL, { small: true });
+    }
   })
 
   done()
